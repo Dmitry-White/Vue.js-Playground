002e640ed5218be695f2e19d5e755d239aee82bc
Dzmitry_Belaichuk <dzmitry_belaichuk@epam.com>
2018-09-13 16:44:48 +0300

    Computed properties: issue outline

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ new Vue({
     el: '#app',
     data: {
         counter: 0,
-        result: ''
     },
     methods: {
-        increase() {
-            this.counter++;
-            this.result = this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
+        result() {
+            return this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
         }
     }
 });
