1290cbe70657201d924ca1a11b7b2744514fe1c3
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-03-04 09:38:48 +0300

    Add animate.css, bootstrap and fontawesome libraries

--- vue.js-essential-training/src/main.js
+++ vue.js-essential-training/src/main.js
@@ -1,5 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import bootstrap from 'bootstrap';
+import css from 'bootstrap/dist/css/bootstrap.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import animate from 'animate.css';
+
+import { faShoppingCart, faDollarSign } from '@fortawesome/free-solid-svg-icons';
+
+import App from './App.vue';
+
+library.add(faShoppingCart, faDollarSign);
 
 Vue.config.productionTip = false
 
