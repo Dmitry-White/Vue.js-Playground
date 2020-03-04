7656a3e0d8c950338e59426635794a38a91118c5
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-03-04 09:48:46 +0300

    Add shopping cart icon test

--- vue.js-essential-training/src/App.vue
+++ vue.js-essential-training/src/App.vue
@@ -1,23 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="container mt-5">
+    <h1>My Shop</h1>
+    <p class="animated fadeInRight">Take a look at our offerings below</p>
+    <FontAwesomeIcon icon="shopping-cart" />
   </div>
 </template>
 
 <script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    FontAwesomeIcon
+  }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- vue.js-essential-training/src/main.js
+++ vue.js-essential-training/src/main.js
@@ -1,9 +1,8 @@
 import Vue from 'vue';
-import bootstrap from 'bootstrap';
-import css from 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'animate.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import animate from 'animate.css';
-
 import { faShoppingCart, faDollarSign } from '@fortawesome/free-solid-svg-icons';
 
 import App from './App.vue';
