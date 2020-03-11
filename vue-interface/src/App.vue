163f57c99de036cc45d945367c3dbf27a7fda651
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-03-11 17:19:20 +0300

    Add list of appointments

--- vue-interface/src/App.vue
+++ vue-interface/src/App.vue
@@ -1,8 +1,13 @@
 <template>
-  <div id="main-app" class="container">
-    <h4>{{ title }}</h4>
-    <FontAwesomeIcon icon="plus" class="mr-2"/>Add Appointment
-  </div>
+  <section id="main-app" class="container">
+    <h3>{{ title }}</h3>
+    <FontAwesomeIcon icon="plus" class="mr-2" />Add Appointment
+
+    <div v-for="(item, index) in appointments" :key="index">
+      <h4>{{item.petName}}</h4>
+      <p>{{item.aptNotes}}</p>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -13,6 +18,33 @@ export default {
   data() {
     return {
       title: "Appointment List",
+      appointments: [
+        {
+          petName: "Spot",
+          petOwner: "Constance Smith",
+          aptDate: "2017-07-24 08:30",
+          aptNotes: "This German Shepherd is having some back pain",
+        },
+        {
+          petName: "Goldie",
+          petOwner: "Barot Bellingham",
+          aptDate: "2017-07-22 15:50",
+          aptNotes: "This Goldfish has some weird spots in the belly",
+        },
+        {
+          petName: "Mitten",
+          petOwner: "Hillary Goldwyn",
+          aptDate: "2017-07-21 9:15",
+          aptNotes: "Cat has excessive hairballs",
+        },
+        {
+          petName: "Buffy",
+          petOwner: "Hassum Harrod",
+          aptDate: "2017-07-20 15:30",
+          aptNotes:
+            "This Chihuahua has not eaten for three days and is lethargic",
+        },
+      ],
     };
   },
   components: {
