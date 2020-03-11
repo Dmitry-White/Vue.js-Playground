6bb0e048d138e9c5e6df487280f60afd01862a2b
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-03-11 17:11:21 +0300

    Add fontawesome plus icon

--- vue-interface/src/App.vue
+++ vue-interface/src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="main-app" class="container">
     <h4>{{ title }}</h4>
+    <FontAwesomeIcon icon="plus" class="mr-2"/>Add Appointment
   </div>
 </template>
 
 <script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
 export default {
   name: "MainApp",
   data() {
     return {
       title: "Appointment List",
     };
+  },
+  components: {
+    FontAwesomeIcon,
   },
 };
 </script>
