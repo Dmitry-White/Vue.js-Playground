683a015e2962f65129814413b5238601ccac60f5
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-03-12 19:25:16 +0300

    Move appointments to a separate component

--- vue-interface/src/App.vue
+++ vue-interface/src/App.vue
@@ -3,16 +3,15 @@
     <h3>{{ title }}</h3>
     <FontAwesomeIcon icon="plus" class="mr-2" />Add Appointment
 
-    <div v-for="(item, index) in appointments" :key="index">
-      <h4>{{ item.petName }}</h4>
-      <p>{{ item.aptNotes }}</p>
-    </div>
+    <AppointmentList :appointments="appointments" />
   </section>
 </template>
 
 <script>
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import axios from "axios";
+
+import AppointmentList from "./components/AppointmentList";
 
 export default {
   name: "MainApp",
@@ -24,11 +23,12 @@ export default {
   },
   components: {
     FontAwesomeIcon,
+    AppointmentList,
   },
   mounted() {
     axios
       .get("./data/appointments.json")
-      .then((res) => this.appointments = res.data)
+      .then((res) => (this.appointments = res.data))
       .catch((err) => console.error(err));
   },
 };
---
+++ vue-interface/src/components/AppointmentList.vue
@@ -0,0 +1,17 @@
+<template>
+  <section id="appointments">
+    <div v-for="(item, index) in appointments" :key="index">
+      <h4>{{ item.petName }}</h4>
+      <p>{{ item.aptNotes }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "AppointmentList",
+  props: ["appointments"],
+};
+</script>
+
+<style></style>
