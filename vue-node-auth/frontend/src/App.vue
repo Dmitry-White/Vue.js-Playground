4b513014a7c3a2827447a2d515ced09db08562de
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-03-14 16:48:12 +0300

    Add dummy Messages component

--- vue-node-auth/frontend/src/App.vue
+++ vue-node-auth/frontend/src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Messages />
+  </div>
 </template>
 
 <script>
+import Messages from "./components/Messages";
 export default {
   name: "App",
+  components: {
+    Messages,
+  },
 };
 </script>
 
