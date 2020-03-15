2c0749fbcbb44b506753cbea47ca31d55eaa54e2
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-03-15 13:06:42 +0300

    Add messages endpoint and nodemon

--- vue-node-auth/backend/server.js
+++ vue-node-auth/backend/server.js
@@ -3,6 +3,10 @@ import express from 'express';
 const PORT = 3000;
 const app = express();
 
-app.get('/', (_, res) => res.send('Test'));
+app.get('/messages', (_, res) => {
+  const messages = ["hello", "hi"];
+
+  res.send(messages);
+});
 
 app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
