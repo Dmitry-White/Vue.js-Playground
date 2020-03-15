5803d59bde64331b0b7034d3b1486160ecef57cb
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-03-15 18:13:58 +0300

    Add vuetify

--- vue-node-auth/frontend/src/App.vue
+++ vue-node-auth/frontend/src/App.vue
@@ -1,15 +1,25 @@
 <template>
-  <div id="app">
-    <Messages />
-  </div>
+  <v-app>
+    <v-app-bar app> </v-app-bar>
+
+    <v-content>
+      <Messages />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 import Messages from "./components/Messages";
+
 export default {
   name: "App",
+
   components: {
     Messages,
   },
+
+  data: () => ({
+    //
+  }),
 };
 </script>
