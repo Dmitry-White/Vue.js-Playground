c50ee4e6f4334e1f324ed5567f84441c623d56dc
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-03-16 15:59:29 +0300

    Add body-parser middleware

--- vue-node-auth/backend/server.js
+++ vue-node-auth/backend/server.js
@@ -1,15 +1,24 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const PORT = 3000;
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/messages', (_, res) => {
   const messages = ["yes", "no", "s*x", "I dunno"];
 
   res.send(messages);
+});
+
+app.post('/messages', (req, res) => {
+  const { body } = req;
+  console.log(body);
+
+  res.json(body);
 });
 
 app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
