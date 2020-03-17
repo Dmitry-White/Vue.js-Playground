d4aaa1a702007b5ec089eee53bb7469188f9e71e
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-03-17 20:46:08 +0300

    Add toolbar titles and styling

--- vue-node-auth/frontend/src/components/Messages.vue
+++ vue-node-auth/frontend/src/components/Messages.vue
@@ -1,15 +1,23 @@
 <template>
-  <div>
-    <h4 class="display-1">Messages</h4>
+  <v-flex sm8 offset-sm2>
+    <v-card>
+      <v-toolbar dark>
+        <v-toolbar-title>Messages</v-toolbar-title>
+      </v-toolbar>
 
-    <v-list>
-      <v-list-item v-for="(message, i) in messages" :key="i" @click="() => {}">
-        <v-list-item-content>
-          <v-list-item-title v-text="message"></v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
-  </div>
+      <v-list>
+        <v-list-item
+          v-for="(message, i) in messages"
+          :key="i"
+          @click="() => {}"
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="message"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-card>
+  </v-flex>
 </template>
 
 <script>
--- vue-node-auth/frontend/src/components/NewMessage.vue
+++ vue-node-auth/frontend/src/components/NewMessage.vue
@@ -1,6 +1,9 @@
 <template>
   <v-flex sm8 offset-sm2>
     <v-card>
+      <v-toolbar dark>
+        <v-toolbar-title>New Message</v-toolbar-title>
+      </v-toolbar>
       <v-form>
         <v-container>
           <v-row>
