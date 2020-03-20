8f764c95ac43d6a166b3345646f0d32563feac6e
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-03-20 18:37:38 +0300

    Add register component

---
+++ vue-node-auth/frontend/src/components/Register.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-flex sm8 offset-sm2>
+    <v-card>
+      <v-toolbar dark>
+        <v-toolbar-title>Register</v-toolbar-title>
+      </v-toolbar>
+      <v-form @submit.prevent="register">
+        <v-container>
+          <v-row>
+            <v-col cols="12" md="4">
+              <v-text-field
+                label="User Name"
+                v-model="username"
+                required
+              ></v-text-field>
+              <v-text-field
+                label="Password"
+                v-model="password"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+        <v-btn type="submit">register</v-btn>
+      </v-form>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  name: "Register",
+  data: () => ({
+    username: "",
+    password: "",
+  }),
+  methods: {
+    register() {
+      const data = {
+        username: this.username,
+        password: this.password,
+      };
+
+      this.$store.dispatch("register", data);
+    },
+  },
+};
+</script>
+
+<style></style>
--- vue-node-auth/frontend/src/plugins/router.js
+++ vue-node-auth/frontend/src/plugins/router.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router';
 import Messages from '../components/Messages.vue';
 import NewMessage from '../components/NewMessage.vue';
 import Message from '../components/Message.vue';
+import Register from '../components/Register.vue';
 
 Vue.use(VueRouter);
 
@@ -11,7 +12,8 @@ const routerOptions = {
   routes: [
     { path: '/', component: Messages },
     { path: '/new-message', component: NewMessage },
-    { path: '/message/:id', component: Message }
+    { path: '/message/:id', component: Message },
+    { path: '/register', component: Register },
   ],
   mode: 'history',
 }
