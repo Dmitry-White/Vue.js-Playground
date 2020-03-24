d037c209d524f9760dfc1e88a0d1e27b8d85fe4b
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-03-24 17:33:07 +0300

    Add home view component

--- vue-firebase/src/components/Navigation.vue
+++ vue-firebase/src/components/Navigation.vue
@@ -4,7 +4,7 @@
       <router-link class="navbar-brand" to="/">Meeting Log</router-link>
       <div class="navbar-nav ml-auto">
         <router-link class="nav-item nav-link" to="/meetings">Meetings</router-link>
-        <router-link class="nav-item nav-link" to="/login">Login</router-link>
+        <router-link class="nav-item nav-link" to="/login">Log In</router-link>
         <router-link class="nav-item nav-link" to="/register">Register</router-link>
       </div>
     </div>
