973bc790bda6e501d4f8e966ef83e692d3c610dd
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2020-03-30 23:55:00 +0300

    Add firebase setup

--- vue-firebase/src/views/Home.vue
+++ vue-firebase/src/views/Home.vue
@@ -1,41 +1,56 @@
 <template>
-  <div class="container text-center">
-    <div class="row justify-content-center">
-      <div class="col-10 col-md-10 col-lg-8 col-xl-7">
-        <div class="display-4 text-primary mt-3 mb-2">
-          Meeting Log
-        </div>
-        <p class="lead">
-          This simple app creates meetings, allows people to check in, and picks
-          random users to award giveaways. It's a good example of a Single Page
-          Application which includes connection to a database and routing. It's
-          a practical way to learn <a href="https://vuejs.org/">Vue.js</a> with
-          <a href="https://firebase.google.com">Firebase</a>.
-          <FontAwesomeIcon class="text-danger" icon="star" />
-        </p>
+  <div class="mt-3">
+    <div class="text-secondary text-center">
+      Welcome back
+      <span class="text-weight-bold text-info">{{ user }}</span>
+    </div>
+    <div class="container text-center">
+      <div class="row justify-content-center">
+        <div class="col-10 col-md-10 col-lg-8 col-xl-7">
+          <div class="display-4 text-primary mb-2">
+            Meeting Log
+          </div>
+          <p class="lead">
+            This simple app creates meetings, allows people to check in, and
+            picks random users to award giveaways. It's a good example of a
+            Single Page Application which includes connection to a database and
+            routing. It's a practical way to learn
+            <a href="https://vuejs.org/">Vue.js</a> with
+            <a href="https://firebase.google.com">Firebase</a>.
+          </p>
 
-        <router-link to="/register" class="btn btn-outline-primary mr-2">
-          Register
-        </router-link>
-        <router-link to="/login" class="btn btn-outline-primary mr-2">
-          Log In
-        </router-link>
-        <router-link to="/meetings" class="btn btn-primary">
-          Meetings
-        </router-link>
+          <router-link to="/register" class="btn btn-outline-primary mr-2">
+            Register
+          </router-link>
+          <router-link to="/login" class="btn btn-outline-primary mr-2">
+            Log In
+          </router-link>
+          <router-link to="/meetings" class="btn btn-primary">
+            Meetings
+          </router-link>
+        </div>
+        <!-- columns -->
       </div>
-      <!-- columns -->
     </div>
   </div>
 </template>
 
 <script>
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import db from "../db";
 
 export default {
   name: "Home",
-  components: {
-    FontAwesomeIcon
-  }
+  data: () => ({
+    user: null,
+  }),
+  mounted() {
+    db.collection("users")
+      .doc("l1ImCfNOm79zD0V7gkTa")
+      .get()
+      .then((res) => {
+        this.user = res.data().name;
+      })
+      .catch((err) => console.log(err));
+  },
 };
 </script>
